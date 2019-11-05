e2ddeda00b51e2965fd6d51c1516bcef987ab561
JanHenst <42889823+JanHenst@users.noreply.github.com>
2019-11-05 21:11:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+
+let modaal = document.querySelector('#modaalAchtergrond');
+let modaalVenster = document.querySelector('#modaalVenster');
+let modaalInhoud = document.querySelector('#modaalInhoud');
+let triggers = document.querySelectorAll('.trigger');
+let sluitKnop = document.getElementById('sluitKnop');
+let arrow = document.getElementById('arrow');
+let modaleInhouden = new Object();
+
+const sluitModaal = () => {
+  modaal.parentNode.removeChild(modaal);
+  modaalVenster.innerHTML = '';
+}
+
+modaalVenster.addEventListener('click', (e) => {
+  e.stopPropagation();
+})
+
+sluitKnop.addEventListener('click', sluitModaal);
+modaal.addEventListener('click',sluitModaal);
+
+for(let i=0; i<triggers.length; i++){
+  let item = triggers[i].hash.substr(1);
+  modaleInhouden[item] = document.getElementById(item);
+  triggers[i].addEventListener('click', (e)=> {
+    e.preventDefault();
+    modaalVenster.appendChild(sluitKnop);
+    modaalVenster.appendChild(modaleInhouden[item]);
+    document.body.appendChild(modaal);
+
+  })
+}
+
+for(elem in modaleInhouden){
+  modaleInhouden[elem].parentNode.removeChild(modaleInhouden[elem]);
+}
+
+sluitModaal();
